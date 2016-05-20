87d5e429bb769630ef7940562dd7ebed97b1cccb
Yanchick <yan@yanchick.org>
2016-05-20 20:45:12 +0500

    Correct Cmake file and add readme

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@ int main( int argc, char **argv )
 
     QPushButton hello( "Hello world!", 0 );
     hello.resize( 100, 30 );
-
-    //a.setMainWidget( &hello );
     hello.show();
+
     return a.exec();
 }
