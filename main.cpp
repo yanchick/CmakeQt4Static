6e31476a446166501d9f69aa64d827cf50bca655
Yanchick <yan@yanchick.org>
2016-05-20 10:09:42 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <Qt/qapplication.h>
+#include <QtGui/qpushbutton.h>
+
+
+int main( int argc, char **argv )
+{
+    QApplication a( argc, argv );
+
+    QPushButton hello( "Hello world!", 0 );
+    hello.resize( 100, 30 );
+
+    //a.setMainWidget( &hello );
+    hello.show();
+    return a.exec();
+}
